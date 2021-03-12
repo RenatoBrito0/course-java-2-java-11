221e371ea1904ccd82c0eb025a0d5c89e721797a
RenatoBrito0 <renatoab12@gmail.com>
2021-03-11 23:08:49 -0300

    H2 database, test profile, JPA

--- src/main/java/com/brito/course/entities/User.java
+++ src/main/java/com/brito/course/entities/User.java
@@ -2,9 +2,17 @@ package com.brito.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY) //auto-increment no bd
 	private Long id;
 	private String name;
 	private String email;
@@ -87,5 +95,7 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
+
+	
 
 }
